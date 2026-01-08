556d31bacbcc94e130ff7bd921db4f4560942670
Diegton Rodrigues <diegton@gmail.com>
2026-01-08 14:25:42 -0300

    selected date style

--- components/dates/dates-component.js
+++ components/dates/dates-component.js
@@ -1,22 +1,33 @@
 import { months, weekDays } from '../../dates-utils.js'
 export default {
     template: `#dates-template`,
+    props: ['date'],
     data() {
         return {
-            dates: []
+            dates: [],
         }
     },
     beforeMount() {
+        console.log(this.date)
         const tenElements = Array.from(Array(10).keys())
         this.dates = tenElements.map(el => {
-            const today = new Date()
+            const today = this.date ? new Date(this.date) : new Date()
             const days = today.setDate(today.getDate() + (el - 5))
             const elementDate = new Date(days)
+            const stringDate = elementDate.toISOString().split("T")[0]
+            const cardClass = []
+            if(stringDate == (new Date()).toISOString().split("T")[0]){
+                cardClass.push("date-today" )
+            }
+            if (stringDate == this.date){
+                cardClass.push("date-selected")
+            }
             return {
                 day: elementDate.getDate(),
                 month: months[elementDate.getMonth() + 1],
                 weekDay: weekDays[elementDate.getDay()],
-                stringDate: elementDate.toISOString().split("T")[0]
+                stringDate,
+                cardClass: cardClass.join(" ")
             }
         })
     },
